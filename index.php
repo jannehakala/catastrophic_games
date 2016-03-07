ee2e3df68df81292eadef24665a511e4689864a6
TeemuTT <tuomela.teemu@gmail.com>
2016-03-07 15:17:54 +0200

    composer

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
 
-echo "Hello!";
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Catastrophic_Games</title>
+</head>
 
-?>
+<body>
+    <?php
+    echo "Hello heroku!<br>";
+    echo date("Y-m-d");
+    ?>
+</body>
+</html>
+
