7541c21242c418d465a3a7afa933f27d8e3d6b0b
jannehakala <janne.hakala94@hotmail.fi>
2016-03-09 15:18:02 +0200

    init

--- init.php
+++ init.php
@@ -5,11 +5,14 @@ $db = pg_connect("host=ec2-54-83-56-31.compute-1.amazonaws.com dbname=d9m4a5line
 
 // Performing SQL query
 
+
+
    $sql =<<<EOF
-      CREATE TABLE USERS
+      CREATE TABLE EXECATEGORY
       (ID INT PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      ADDRESS        CHAR(20)	NOT NULL);
+      CODE           TEXT    NOT NULL,
+      NAME       	 TEXT	NOT NULL);
+
 EOF;
 
 $ret = pg_query($db, $sql);
