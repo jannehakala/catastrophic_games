65ef7613b01bb3dad5b2a698b9b519dcfb3355c2
TeemuTT <tuomela.teemu@gmail.com>
2016-03-09 12:08:28 +0200

    prototyping layout

---
+++ main.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Scarabeus</title>
+	<link rel="stylesheet" type="text/css" href="mainstyles.css">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<h1>Welcome</h1>
+			<select>
+				<option>Profile</option>
+			</select>
+		</div>
+
+		<div id="main">
+			<div id="left">
+				<div id="left-1">
+					<h2>Exercises</h2>
+					<ul>
+						<li>Exercise 1</li>
+						<li>Exercise 2</li>
+						<li>Exercise 3</li>
+					</ul>
+				</div>
+				<div id="left-2">
+					<h2>Statistics</h2>
+				</div>
+			</div>
+
+			<div id="right">
+				<div id="content">
+					<h2>Quiz</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor a nisi sit amet aliquet. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque ullamcorper tellus turpis, non maximus orci hendrerit eu. Suspendisse eu erat a dui mattis euismod. Donec eleifend gravida dolor ut ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in venenatis ex. Pellentesque aliquet odio nec eros auctor facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+					</p>
+				</div>
+			</div>
+		</div>		
+	</div>
+</body>
+</html>
