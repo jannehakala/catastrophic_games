04b30e006ae2c48ed179737f9e77d0bd7d43a817
jannehakala <janne.hakala94@hotmail.fi>
2016-03-09 15:13:25 +0200

    init

--- init.php
+++ init.php
@@ -4,19 +4,12 @@ $db = pg_connect("host=ec2-54-83-56-31.compute-1.amazonaws.com dbname=d9m4a5line
     or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
+
    $sql =<<<EOF
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (1, 'Paul', 32, 'California', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
-	   SELECT * from COMPANY;
+      CREATE TABLE USERS
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      ADDRESS        CHAR(20)	NOT NULL);
 EOF;
 
 $ret = pg_query($db, $sql);
@@ -25,13 +18,7 @@ $ret = pg_query($db, $sql);
    } else {
       echo "taulu luotiin jes\n";
    }
-   while($row = pg_fetch_row($ret)){
-      echo "ID = ". $row[0] . "\n";
-      echo "NAME = ". $row[1] ."\n";
-      echo "ADDRESS = ". $row[2] ."\n";
-      echo "SALARY =  ".$row[4] ."\n\n";
-   }
-   echo "Operation done successfully\n";
+
 
 pg_close($dbconn);
 
