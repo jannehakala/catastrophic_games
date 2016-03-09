31a8944ee37af716cab31e0c2f41831204fe6d43
VesaKivisto <kivisto.vesku@gmail.com>
2016-03-09 12:13:57 +0200

    layout

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
+<?php
+include('login.php'); // Includes Login Script
 
+if(isset($_SESSION['login_user'])){
+header("location: profile.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Catastrophic_Games</title>
+<title>Login Form in PHP with Session</title>
+<link href="style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-    <?php
-    echo "Hello heroku!<br>";
-    echo date("Y-m-d");
-    ?>
+<div id="main">
+<h1>PHP Login Session Example</h1>
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>UserName :</label>
+<input id="name" name="username" placeholder="username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password">
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</div>
 </body>
 </html>
-
