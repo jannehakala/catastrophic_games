88f90d66bf819bb9974176556115dc0bb600e924
jannehakala <janne.hakala94@hotmail.fi>
2016-03-09 15:07:37 +0200

    init

--- init.php
+++ init.php
@@ -4,16 +4,13 @@ $db = pg_connect("host=ec2-54-83-56-31.compute-1.amazonaws.com dbname=d9m4a5line
     or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
-
-
-
-
-
    $sql =<<<EOF
-      CREATE TABLE USER
+      CREATE TABLE COMPANY
       (ID INT PRIMARY KEY     NOT NULL,
       NAME           TEXT    NOT NULL,
-      PASSWORD        CHAR(20) NOT NULL);
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
 EOF;
 
 $ret = pg_query($db, $sql);
