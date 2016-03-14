473e8ee8b982c776248bb540f1acf62dfd1b891a
jannehakala <janne.hakala94@hotmail.fi>
2016-03-14 15:40:35 +0200

    dbinit

--- dbinit.php
+++ dbinit.php
@@ -1,6 +1,6 @@
 <?php
 // Connecting, selecting database
-$db = pg_connect("host=EVL['HOST_KEY] dbname=EVL['DB_KEY'] user=EVL['USER_KEY'] password=EVL['PASSWORD_KEY'")
+$db = pg_connect(":host=>EVL['HOST_KEY], :dbname=>EVL['DB_KEY'], :user=>EVL['USER_KEY'], :password=>EVL['PASSWORD_KEY'")
     or die('Could not connect: ' . pg_last_error());
 
 
