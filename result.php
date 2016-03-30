e83b8e6bd7fb610cbb2dddb99b979471530e23f7
Pewpo <samulir@windowslive.com>
2016-03-30 13:52:28 +0300

    oijhyt

--- result.php
+++ result.php
@@ -4,7 +4,7 @@
 session_start();
 require_once("BL.php");
 echo "<h1>Your score: " . $_SESSION['score'] . "</h1><br>";
-set_stats($_SESSION['username'], 'drugcalculations', $_SESSION['score']);
+set_stats($_SESSION['login_user'], 'drugcalculations', $_SESSION['score']);
 $_SESSION['score'] = 0;
 
 ?>
