e7e63d01a1fcad4a204741c0dcb35590b9fc562d
Pewpo <samulir@windowslive.com>
2016-03-30 14:10:48 +0300

    asd

--- BL.php
+++ BL.php
@@ -8,21 +8,21 @@ require_once("dbinit.php");
 	$sql = <<<SQL
 	INSERT INTO exercise_user(points, "User_id", "Exercise_id") VALUES($value,(select id from users where name = '$username'), (select id from exercise where info = '$exercise'));		
 SQL;
-
 	$result = pg_query($db, $sql);	
 }
 function get_stats($username){ 
- //pohhjaaaa
- /*echo "<table>\n"; 
+require_once("dbinit.php");
+	echo "<table>\n"; 
 	echo '<thead>
-		<tr>
-		<th>USERID</th>
-		</tr>
-		</thead>
-		<tbody>';
-	$stmt = pg_query($this->db, "select id from users where name = '" . $username ."'");
-	$row = pg_fetch_row($stmt);
-	$stmt = pg_query($this->db, "select* from exercise_user where name = '" . $row[0] ."'");
+			<tr>
+			<th>USERID</th>
+			</tr>
+			</thead>
+			<tbody>';
+	$sql = <<<SQL
+	select points from exercise_user where "User_id" = (select id from users where name = '$username');
+SQL;
+	$stmt = pg_query($db, "select* from exercise_user where  = (select id from users where name = '" . $username ."')");
 	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     	echo "<tr><td>{$row['User_id']}</td><td>{$row['points']}</td></tr>\n";
 	}
