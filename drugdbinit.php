bc0d00d37eade87f8205ab5d8fea24ba61105ec9
TeemuTT <tuomela.teemu@gmail.com>
2016-03-30 11:00:03 +0300

    invalid charset

--- drugdbinit.php
+++ drugdbinit.php
@@ -6,7 +6,7 @@ $dbname = getenv('MYSQL_DB_NAME');
 $user = getenv('MYSQL_USER');
 $pw = getenv('MYSQL_PW');
 
-$db_mysql = new PDO("mysql:host={$host};dbname={$dbname};charset=utf-8", $user, $pw);
+$db_mysql = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pw);
 
 $db_mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db_mysql->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
