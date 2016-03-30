06fe757f4c83cbc136337db00165e349086c1593
Pewpo <samulir@windowslive.com>
2016-03-30 13:31:44 +0300

    a

--- BL.php
+++ BL.php
@@ -5,7 +5,7 @@ function set_stats(){
 	$exercise = 'drugcalculations';
 	$value = 2;
 	$sql = <<<SQL
-	INSERT INTO exercise_user(points, "User_id", "Exercise_id") VALUES($value,(select id from users where name = '$username'), (select id from exercise where info = '$exercise'))		
+	INSERT INTO exercise_user(points, "User_id", "Exercise_id") VALUES($value,(select id from users where name = '$username'), (select id from exercise where info = '$exercise'));		
 SQL;
 
 	$result = pg_query($this->db, $sql);	
