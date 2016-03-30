7eccf7d8492b9ef0e19b8fef1497c2f90d45e846
TeemuTT <tuomela.teemu@gmail.com>
2016-03-30 10:45:25 +0300

    drugdbinit

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 include("header.php");
-require_once("drugdbinit.php");
+include("drugdbinit.php");
 
 ?>
 		<div id="main">
