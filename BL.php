4aa424e18b6d7ae903c853dfa92e81da94916b61
Pewpo <samulir@windowslive.com>
2016-03-30 13:39:44 +0300

    require dbinit

--- BL.php
+++ BL.php
@@ -2,8 +2,9 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
-
+require_once("dbinit.php");
 function set_stats(){
+
 	$username= 'Teemu';
 	$exercise = 'drugcalculations';
 	$value = 2;
