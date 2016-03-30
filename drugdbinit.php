d16b7f84602531bf1e9cd8da99f0108bdae212a9
TeemuTT <tuomela.teemu@gmail.com>
2016-03-30 10:50:20 +0300

    drugdbinit

--- drugdbinit.php
+++ drugdbinit.php
@@ -6,7 +6,11 @@ $dbname = getenv('MYSQL_DB_NAME');
 $user = getenv('MYSQL_USER');
 $pw = getenv('MYSQL_PW');
 
-$db_mysql = new PDO('mysql:host=$host;dbname=$dbname;charset=utf-8', $user, $pw);
+try {
+    $db_mysql = new PDO('mysql:host={$host};dbname={$dbname};charset=utf-8', $user, $pw);
+} catch (Exception $ex) {
+    echo $ex->getMessage();
+}
 
 $db_mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db_mysql->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
