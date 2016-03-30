9cb98a2698e79b62f576bd1ea95339babeeb9e2c
Pewpo <samulir@windowslive.com>
2016-03-30 14:15:02 +0300

    sa

--- BL.php
+++ BL.php
@@ -24,7 +24,7 @@ require_once("dbinit.php");
 SQL;
 	$stmt = pg_query($db, $sql);
 	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    	echo "<tr><td>{$row['User_id']}</td><td>{$row['points']}</td></tr>\n";
+    	echo "<tr><td>".$row['User_id']."</td><td>".$row['points']."</td></tr>\n";
 	}
 	echo "</tbody></table>";
 }
