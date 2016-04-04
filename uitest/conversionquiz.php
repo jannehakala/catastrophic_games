d9aea6e5464aca63da77cdda9eb1f4727005906e
VesaKivisto <kivisto.vesku@gmail.com>
2016-04-04 15:54:11 +0300

    Fixing quizes

--- uitest/conversionquiz.php
+++ uitest/conversionquiz.php
@@ -1,22 +1,43 @@
-
 <?php
 include ("header.php");
+include ("BL.php");
 ?>
-            <div id="right">
-                <div id="content">
-                    <h2>Unit conversions.</h2>
-                    <p style="margin-top: 20px;">
-                        Unit conversions.
-                    </p>        
-                    <p style="margin-top: 20px;">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-9">
+					<div class="jumbotron">
+						<h2>Unit conversions</h2>
+						<p>Unit conversions</p>
+						
+						<p>
                         <form method="POST" action="quizview.php">
-                            <input type="submit" value="Start">
-                            <input type="hidden" name="quiztype" value="unitconversionquiz">
+                            <input type="submit" value="Start exercise">
+                            <input type="hidden" name="quiztype" value="laskuquiz">
                         </form>
                     </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="exercises">
+						<h2>Exercises</h2>
+						<ul>
+							<li><a href="llquiz.php">Drug identification</a></li>
+							<li><a href="laskuquiz.php">Drug calculations</a></li>
+							<li><a href="conversionquiz.php">Unit conversions</a></li>
+						</ul>
+					</div>
+					<h2>Statistics</h2>
+					<?php get_stats($_SESSION['login_user'],0); ?>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
 </html>
--- uitest/llquiz.php
+++ uitest/llquiz.php
@@ -1,22 +1,43 @@
-
 <?php
 include ("header.php");
+include ("BL.php");
 ?>
-            <div id="right">
-                <div id="content">
-                    <h2>Drug identification</h2>
-                    <p style="margin-top: 20px;">
-                        Drug identification
-                    </p>        
-                    <p style="margin-top: 20px;">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-9">
+					<div class="jumbotron">
+						<h2>Drug identifications</h2>
+						<p>Drug identifications</p>
+						
+						<p>
                         <form method="POST" action="quizview.php">
-                            <input type="submit" value="Start">
-                            <input type="hidden" name="quiztype" value="drugidentificationquiz">
+                            <input type="submit" value="Start exercise">
+                            <input type="hidden" name="quiztype" value="ainequiz">
                         </form>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+						</p>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="exercises">
+						<h2>Exercises</h2>
+						<ul>
+							<li><a href="llquiz.php">Drug identification</a></li>
+							<li><a href="laskuquiz.php">Drug calculations</a></li>
+							<li><a href="conversionquiz.php">Unit conversions</a></li>
+						</ul>
+					</div>
+					<h2>Statistics</h2>
+					<?php get_stats($_SESSION['login_user'],0); ?>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
 </html>
