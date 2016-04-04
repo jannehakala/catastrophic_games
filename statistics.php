e473898653e15aa6cab3bc8209d397aaba171fe6
Pewpo <samulir@windowslive.com>
2016-04-04 13:15:53 +0300

    add statistics page

---
+++ statistics.php
@@ -0,0 +1,18 @@
+<?php
+
+include("header.php");
+include("drugdbinit.php");
+
+?>
+            <div id="right">
+                <div id="content">
+                    <h2>Scarabeus</h2>
+                    <p>
+                        Your statistics
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
