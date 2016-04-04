025bc550d14b597dde8105ac2bd91cb115aa3016
Pewpo <samulir@windowslive.com>
2016-04-04 14:09:57 +0300

    asd

--- statistics.php
+++ statistics.php
@@ -8,31 +8,10 @@ include("drugdbinit.php");
             <div id="right">
                 <div id="content">
                     <h2>Your stats</h2>                    
-                       <?php get_stats($_SESSION['login_user'], 1);?>      
-					 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-					<script type="text/javascript">
-					  google.charts.load('current', {'packages':['corechart']});
-					  google.charts.setOnLoadCallback(drawChart);
+                       <?php get_stats($_SESSION['login_user'], 1);
+							tee_graafi(13,12,14);
+					   ?>      
 
-					  function drawChart() {
-						var data = google.visualization.arrayToDataTable([
-						  ['Year', 'Sales', 'Expenses'],
-						  ['2004',  1000,      400],
-						  ['2005',  1170,      460],
-						  ['2006',  660,       1120],
-						  ['2007',  1030,      540]
-						]);
-
-						var options = {
-						  title: 'Company Performance',
-						  curveType: 'function',
-						  legend: { position: 'bottom' }
-						};
-
-						var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-						chart.draw(data, options);
-					  }
 					</script>					   
                 </div>
             </div>
@@ -40,3 +19,28 @@ include("drugdbinit.php");
     </div>
 </body>
 </html>
+<?php function tee_graafi($aanimaara0,$aanimaara1,$aanimaara2 )
+{
+ echo  ' <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ["Task", "Hours per Day"],
+          ["pinaatti",   '.$aanimaara0.'],
+          ["salaatti",      '.$aanimaara1.'],
+          ["pataatti",  '.$aanimaara2.']
+          ]);
+ 
+        var options = {
+          title: "Mikä on paras",
+          pieHole: 0.4,
+        };
+ 
+        var chart = new google.visualization.PieChart(document.getElementById("donutchart"));
+        chart.draw(data, options);
+      }
+    </script>';
+}
+  ?>
