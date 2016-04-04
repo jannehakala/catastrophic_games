7c6dcdd4b010b475ac5b3fd1ef66777bd17cfde7
Pewpo <samulir@windowslive.com>
2016-04-04 13:52:11 +0300

    asd

--- statistics.php
+++ statistics.php
@@ -2,14 +2,14 @@
 
 include("header.php");
 include("drugdbinit.php");
-include("BL.php")
+
 
 ?>
             <div id="right">
                 <div id="content">
                     <h2>Your stats</h2>
                     <p>
-                        <?php //echo get_stats($_SESSION['login_user'],1);?>
+                       
                     </p>
                 </div>
             </div>
