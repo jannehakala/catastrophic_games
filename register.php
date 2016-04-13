bdbe439d63bc19f532b62f21ec22f731d8016071
VesaKivisto <kivisto.vesku@gmail.com>
2016-04-13 13:17:46 +0300

    Fix

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-//require_once("dbinit.php");
+require_once("dbinit.php");
 require_once("User.class.php");
 $user = new User($db);
 $nameErr = "";
