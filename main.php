ac77f4c7c303514f0011cfc932313fd060d5e62e
pewpo <samulir@windowslive.com>
2016-04-18 13:03:38 +0300

    asd

--- main.php
+++ main.php
@@ -10,7 +10,21 @@ include ("BL.php");
                     <div class="jumbotron">
                         <h2>Scarabeus</h2>
                         <h3>Welcome to Scarabeus learning environment!</h3>
-                        <p>Start now your own learning progress by selecting exercises you want to practice! You can find the exercises in the left panel called "Exercises".  There is three types of exercises: "Drug identification", "Drug calculations" and "Unit conversions". If you want to test your knowledge you can select Exam.  In exam there is questions from all of these three exercise categories mixed up.</p>
+                        <p style="font-size:100%;">Start now your own learning progress by selecting exercises you want to practice!
+                         You can find the exercises in the left panel called "Exercises".
+                           There is three types of exercises: "Drug identification", "Drug calculations" and "Unit conversions". 
+                           If you want to test your knowledge you can select Exam.  
+                           In exam there is questions from all of these three exercise categories mixed up.</p>
+                        <p style="font-size:100%;">
+                        You can also see your statistics from the left panel called "Statistics".
+                         If you press that button it will take you to the statistics page where you can see all your stats.
+                          In this "statistics" page there will be also generated different kind of diagrams which show you yours progresses.</p>
+                          <b>Hints:</b>
+                          <p style="font-size:100%;">Blue house at the left corner will take you back to the main view.  </p>
+                          <p style="font-size:100%;">In green dropdown button you will find selections: "profile" and "log out". 
+                            Profile will take you to the profile page and log out will end your session. </p>
+                            <br><br>
+                            Have fun!
                     </div>
                 </div>
                 <div class="col-md-3">
